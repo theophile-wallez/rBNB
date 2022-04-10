4fddc18f87376f0c57a94846027bb3bc8da7107a
theophilewall <66305625+theophilewall@users.noreply.github.com>
2022-04-10 12:36:14 +0200

    added generated backend template

---
+++ backend/src/main/java/com/psycaptr/rBNB/RBnbApplication.java
@@ -0,0 +1,13 @@
+package com.psycaptr.rBNB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RBnbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RBnbApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/psycaptr/rBNB/RBnbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psycaptr.rBNB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RBnbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
