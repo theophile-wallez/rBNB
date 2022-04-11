ba2bd2fad934a05450766bdf6a76d4d1102816a1
arthur delerue <arthur.delerue91@gmail.com>
2022-04-11 16:44:17 +0200

    Add Firebase structure

---
+++ backend/src/main/java/com/psycaptr/rBNB/Firebase/FBInitialize.java
@@ -0,0 +1,29 @@
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+
+@Service
+public class FBInitialize {
+
+    @PostConstruct
+    public void initialize() {
+        try {
+            FileInputStream serviceAccount =
+                    new FileInputStream("./serviceaccount.json");
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setDatabaseUrl("<DATABASE_URL>")
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
--- backend/src/main/java/com/psycaptr/rBNB/User/UserService.java
+++ backend/src/main/java/com/psycaptr/rBNB/User/UserService.java
@@ -1,7 +1,54 @@
 package com.psycaptr.rBNB.User;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.WriteResult;
+import com.google.firebase.cloud.FirestoreClient;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    public static final String userName="owner";
+
+
     public static User getUserById(Long id) {
         return null;
+    }
+
+    public String savePatientDetails(Patient patient) throws InterruptedException, ExecutionException {
+        Firestore dbFirestore = FirestoreClient.getFirestore();
+        ApiFuture<WriteResult> collectionsApiFuture = dbFirestore.collection(COL_NAME).document(patient.getName()).set(patient);
+        return collectionsApiFuture.get().getUpdateTime().toString();
+    }
+
+    public Patient getPatientDetails(String name) throws InterruptedException, ExecutionException {
+        Firestore dbFirestore = FirestoreClient.getFirestore();
+        DocumentReference documentReference = dbFirestore.collection(COL_NAME).document(name);
+        ApiFuture<DocumentSnapshot> future = documentReference.get();
+
+        DocumentSnapshot document = future.get();
+
+        Patient patient = null;
+
+        if(document.exists()) {
+            patient = document.toObject(Patient.class);
+            return patient;
+        }else {
+            return null;
+        }
+    }
+
+    public String updatePatientDetails(Patient person) throws InterruptedException, ExecutionException {
+        Firestore dbFirestore = FirestoreClient.getFirestore();
+        ApiFuture<WriteResult> collectionsApiFuture = dbFirestore.collection(COL_NAME).document(person.getName()).set(person);
+        return collectionsApiFuture.get().getUpdateTime().toString();
+    }
+
+    public String deletePatient(String name) {
+        Firestore dbFirestore = FirestoreClient.getFirestore();
+        ApiFuture<WriteResult> writeResult = dbFirestore.collection(COL_NAME).document(name).delete();
+        return "Document with Patient ID "+name+" has been deleted";
     }
 }
