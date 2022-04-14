aac91862556230079050fbac93adfdeda89cbc8d
theophilewall <66305625+theophilewall@users.noreply.github.com>
2022-04-14 15:11:26 +0200

    add frontend layout for futures pages

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,9 +5,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateUserComponent } from './create-user/create-user.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PageComponent } from './page/page.component';
+import { ListingComponent } from './listing/listing.component';
 
 @NgModule({
-  declarations: [AppComponent, CreateUserComponent],
+  declarations: [AppComponent, CreateUserComponent, NavbarComponent, PageComponent, ListingComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ frontend/src/app/page/page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'page-container',
+  templateUrl: './page.component.html',
+  styleUrls: ['./page.component.scss'],
+})
+export class PageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
