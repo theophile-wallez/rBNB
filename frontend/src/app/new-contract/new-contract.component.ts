3c1cdca432c522743215d57272dc99057e159060
Julien THOMAS <thomasjulien92140@gmail.com>
2022-04-20 00:55:44 +0200

    linked popup with selected property
    helper service

--- frontend/src/app/new-contract/new-contract.component.ts
+++ frontend/src/app/new-contract/new-contract.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HelperService } from '../helper.service';
 
 @Component({
   selector: 'new-contract',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewContractComponent implements OnInit {
 
-  constructor() { }
+  constructor(public helper: HelperService) { }
 
   ngOnInit(): void {
   }
--- frontend/src/app/popup/popup.component.ts
+++ frontend/src/app/popup/popup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HelperService } from '../helper.service';
 
 @Component({
   selector: 'popup',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PopupComponent implements OnInit {
 
-  constructor() { }
+  constructor(public helper: HelperService) { }
 
   ngOnInit(): void {
   }
