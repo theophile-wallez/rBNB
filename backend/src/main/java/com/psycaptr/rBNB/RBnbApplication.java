ed670fb27569ae1465b8dfaf4f7f5faa6fc04956
Hns Hlr <74055973+Flumxz@users.noreply.github.com>
2022-05-12 17:05:16 +0200

    Mapping: Allowed several methods to avoid CORS errors

--- backend/src/main/java/com/psycaptr/rBNB/RBnbApplication.java
+++ backend/src/main/java/com/psycaptr/rBNB/RBnbApplication.java
@@ -28,7 +28,8 @@ public class RBnbApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200");
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST","PUT", "DELETE");
             }
         };
     }
